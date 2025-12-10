5382bea176d29bd971ff8c168237532b1a46e90b
SuhasK2005 <saisuhask2005@gmail.com>
2025-12-10 21:34:55 +0530

    Started users server side

--- app/routes/admin/all-users.tsx
+++ app/routes/admin/all-users.tsx
@@ -1,14 +1,23 @@
+import { users } from "~/constants";
 import { Header } from "../../../components";
+import { ColumnsDirective, GridComponent, ColumnDirective } from "@syncfusion/ej2-react-grids";
 
 const AllUsers = () => {
   return (
     <main className="dashboard wrapper">
       <Header
-        title="Trips Page"
-        description="Check out our current users in real time."
+        title="Manage Users"
+        description="Filter, sort, and access detailed user profiles"
       />
-
-      All Users Page Contents
+      <GridComponent dataSource={users}>
+        <ColumnsDirective>
+        <ColumnDirective
+         field="name"
+         headerText="Name"
+         width="200"
+         textAlign="Left"/>
+        </ColumnsDirective>
+      </GridComponent>
     </main>
   );
 };
